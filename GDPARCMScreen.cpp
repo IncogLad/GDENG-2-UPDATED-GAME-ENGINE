8dc99476591b315423060350fae5664af8945cef
phipenguu <paul_ong@dlsu.edu.ph>
2023-04-14 20:16:01 +0800

    UI Screen almost done
    just need fps and connect it to the processes

--- GDPARCMScreen.cpp
+++ GDPARCMScreen.cpp
@@ -14,5 +14,55 @@ void GDPARCMScreen::drawUI()
 	ImGui::Begin("GDPARCM Controls", 0, ImGuiWindowFlags_NoResize);
 	ImGui::SetWindowSize(ImVec2(200, 500));
 
+	ImGui::Text("Scene 1");
+	ImGui::ProgressBar(1.0f);
+	if (ImGui::Button("View Scene"))
+	{
+		
+	}
+
+	ImGui::Text("");
+
+	ImGui::Text("Scene 2");
+	ImGui::ProgressBar(1.0f);
+	if (ImGui::Button("View Scene"))
+	{
+		
+	}
+
+	ImGui::Text("");
+
+	ImGui::Text("Scene 3");
+	ImGui::ProgressBar(1.0f);
+	if (ImGui::Button("View Scene"))
+	{
+		
+	}
+
+	ImGui::Text("");
+
+	ImGui::Text("Scene 4");
+	ImGui::ProgressBar(1.0f);
+	if (ImGui::Button("View Scene"))
+	{
+		
+	}
+
+	ImGui::Text("");
+
+	ImGui::Text("Scene 5");
+	ImGui::ProgressBar(1.0f);
+	if (ImGui::Button("View Scene"))
+	{
+		
+	}
+
+	ImGui::Text("");
+
+	if (ImGui::Button("View All Scenes"))
+	{
+		
+	}
+
 	ImGui::End();
 }
